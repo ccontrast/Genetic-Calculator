f76db2167cad580bd6b387259becebd17fcd153e
ccontrast <concurrentcontrast@gmail.com>
2012-08-14 14:15:05 -0700

    percentage calculator returning correct percentages for dominant and co-dominant traits

--- genetic_calc.py
+++ genetic_calc.py
@@ -5,14 +5,14 @@ MORPH_DICT = {"bumblebee":["pastel", "spider"], "pastave":["mojave", "pastel"], 
 
 # checks what morphs you can breed with your current stock
 # gender? -- will be important to check
-def process_collection(collection):
-    collection_list = re.findall(r'\w+', collection)
-    for k,v in MORPH_DICT.items():
-        common = list(set(v) & set(collection_list)
-	if common == MORPH_DICT[k]:
-            print "FOUND: ", k
-	else:
-	    print ("can't create %s") % k
+# def process_collection(collection):
+#     collection_list = re.findall(r'\w+', collection)
+#     for k,v in MORPH_DICT.items():
+#         common = list(set(v) & set(collection_list)
+#         if common == MORPH_DICT[k]:
+#             print "FOUND: ", k
+#         else:
+#             print ("can't create %s") % k
 
 # simple punnett square - calculates what traits may be present in offspring
 # how do i deal with more than two traits? 
@@ -22,15 +22,20 @@ def punnett_square(parents):
 
 # returns percentages for likelyhood of offspring expressing trait
 def trait_percentage(parents):
-    genes = punnett_square(parents)
+    genes = [list(i) for i in punnett_square(parents)]
     total = float(len(genes))
-    for each in genes:
-        percentage = (1/total) * 100
-        print ("%s : %s %s") % (percentage, each[0], each[-1])
+    count = {}
+    for traits in genes:
+        traits.sort()
+        traits = tuple(traits)
+        count[traits] = count.get(traits,0) + 1
+    for k,v in count.items():    
+        percentage = (count[k]/total) * 100
+        print ("%s : %s") % (percentage, k)
 
 # combine with the morph dictionary to further define traits
 # ie pastel pastel is super pastel, pastel mojave is pastave
 
 p = [['pastel','het albino', 'normal'],['pastel','normal']]
 c = "pastel, spider, normal"
-print trait_percentage(p)
+trait_percentage(p)
